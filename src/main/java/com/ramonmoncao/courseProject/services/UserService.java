33471d7d60ee0dc5705a6fe42bff819076235d2e
Ramon Monção <ramonmoncao@hotmail.com>
2024-10-10 20:19:44 -0300

    Exception handler uptade

--- src/main/java/com/ramonmoncao/courseProject/services/UserService.java
+++ src/main/java/com/ramonmoncao/courseProject/services/UserService.java
@@ -13,6 +13,8 @@ import com.ramonmoncao.courseProject.repositories.UserRepository;
 import com.ramonmoncao.courseProject.services.exceptions.DatabaseException;
 import com.ramonmoncao.courseProject.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service //Service component to autowire
 public class UserService {
@@ -45,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity,obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
