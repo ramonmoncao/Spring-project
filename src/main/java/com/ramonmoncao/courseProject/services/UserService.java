aecdeceac8202af51fb5da6eec2f9d7946e3b13c
Ramon Monção <ramonmoncao@hotmail.com>
2024-10-10 20:15:37 -0300

    Exception handler delete

--- src/main/java/com/ramonmoncao/courseProject/services/UserService.java
+++ src/main/java/com/ramonmoncao/courseProject/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ramonmoncao.courseProject.entities.User;
 import com.ramonmoncao.courseProject.repositories.UserRepository;
+import com.ramonmoncao.courseProject.services.exceptions.DatabaseException;
 import com.ramonmoncao.courseProject.services.exceptions.ResourceNotFoundException;
 
 
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id); 
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
